6f8c3d9318aa4f71582223df11a208470edb7da3
Bogdan Stancescu <bogdan.stancescu@rbc.com.ro>
2018-07-09 22:03:52 +0300

    Further code cleanup

--- CDepWinIface/Form1.cs
+++ CDepWinIface/Form1.cs
@@ -38,59 +38,10 @@ namespace ro.stancescu.CDep.CDepWinIface
             dbCfg.AddAssembly("ro.stancescu.CDep.BusinessEntities");
         }
 
-        private void MockupWebRead(object sender, EventArgs e)
-        {
-            WebClient web = new WebClient();
-            var summaryStream = web.OpenRead(@"http://www.cdep.ro/pls/steno/evot2015.xml?par1=1&par2=20180618");
-            using (var summaryReader = new StreamReader(summaryStream))
-            {
-                XmlSerializer summarySerializer = new XmlSerializer(typeof(VoteSummaryCollectionDIO));
-                var summaryData = (VoteSummaryCollectionDIO)summarySerializer.Deserialize(summaryReader);
-            }
-
-            var detailStream = web.OpenRead(@"http://www.cdep.ro/pls/steno/evot2015.xml?par1=2&par2=19974");
-            using (var detailReader = new StreamReader(detailStream))
-            {
-                var detailSerializer = new XmlSerializer(typeof(VoteDetailCollectionDIO));
-                var detailData = (VoteDetailCollectionDIO)detailSerializer.Deserialize(detailReader);
-            }
-        }
-
         private void DBCreate(object sender, EventArgs e)
         {
             var schema = new SchemaExport(dbCfg);
             schema.Create(true, true);
-        }
-
-        private void SQLExec(object sender, EventArgs e)
-        {
-            var sefact = dbCfg.BuildSessionFactory();
-            using (var session = sefact.OpenSession())
-            {
-
-                using (var tx = session.BeginTransaction())
-                {
-                    /*
-                    var students = session.CreateCriteria<Student>().List<Student>();
-                    Console.WriteLine("\nFetch the complete list again\n");
-
-                    foreach (var student in students)
-                    {
-                        Console.WriteLine("{0} \t{1} \t{2} \t{3}", student.ID,
-                           student.FirstName, student.LastName, student.AcademicStanding);
-                    }
-                    */
-                    tx.Commit();
-                }
-
-            }
-
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            SummaryProcessor.Init(dbCfg.BuildSessionFactory());
-            SummaryProcessor.Process(new DateTime(2018, 6, 18));
         }
 
         private void button5_Click(object sender, EventArgs e)
