93654892f1b153bbe2518c37ddc399f5e6b73858
Bogdan Stancescu <bogdan.stancescu@rbc.com.ro>
2018-07-09 20:00:08 +0300

    Timid attempt to use the HtmlAgilityPack (most likely will be abandoned)

---
+++ WebParser/MPProcessor.cs
@@ -0,0 +1,20 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ro.stancescu.CDep.WebParser
+{
+    public class MPProcessor
+    {
+        public void Execute()
+        {
+            var url = "http://www.cdep.ro/pls/parlam/structura2015.de?leg=2016";
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+
+            // div.grup-parlamentar-list
+            //doc.DocumentNode.SelectNodes("")
+        }
+    }
+}
