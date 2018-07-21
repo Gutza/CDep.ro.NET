6d833ae86cdaf9b77ead2890aa12fa0d53fa7613
Bogdan Stancescu <bogdan.stancescu@rbc.com.ro>
2018-07-21 11:54:33 +0300

    Further code refactoring and modularization

--- ScraperLibrary/SenateCalendarScraper.cs
+++ ScraperLibrary/SenateCalendarScraper.cs
@@ -3,6 +3,7 @@ using AngleSharp.Dom;
 using AngleSharp.Dom.Html;
 using AngleSharp.Network.Default;
 using AngleSharp.Parser.Html;
+using NLog;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -16,9 +17,14 @@ namespace ro.stancescu.CDep.ScraperLibrary
     public class SenateCalendarScraper
     {
         private const string baseUrl = "https://www.senat.ro/Voturiplen.aspx";
+        private Logger LocalLogger = null;
 
         public void Execute()
         {
+            if (LocalLogger == null)
+            {
+                LocalLogger = LogManager.GetCurrentClassLogger();
+            }
             _Execute();
         }
 
@@ -26,50 +32,67 @@ namespace ro.stancescu.CDep.ScraperLibrary
         {
             var foo = await GetInitialBrowser();
 
-            (foo.QuerySelector("#__EVENTTARGET") as IHtmlInputElement).Value = "ctl00$B_Center$VoturiPlen1$calVOT";
-            (foo.QuerySelector("#__EVENTARGUMENT") as IHtmlInputElement).Value = "6745";
+            SetDateIndex(foo, "6745");
+            var bar = await SubmitMainForm(foo);
 
-            var fooInner = foo.Body.InnerHtml;
-
-            var bar = await ((IHtmlFormElement)foo.QuerySelector("#aspnetForm")).SubmitAsync();
             var barInner = bar.Body.InnerHtml;
+        }
+
+        private void SetDateIndex(IDocument document, string dateIndex)
+        {
+            SetHtmlEvent(document, "ctl00$B_Center$VoturiPlen1$calVOT", dateIndex);
         }
 
         private async Task<IDocument> GetInitialBrowser()
         {
+            LocalLogger.Trace("Generating the initial browser state");
+
             var requester = new HttpRequester();
             requester.Headers["User-Agent"] = "Mozilla";
 
             // Setup the configuration to support document loading
             var config = Configuration.Default.WithDefaultLoader(requesters: new[] { requester });
+
             // Load the names of all The Big Bang Theory episodes from Wikipedia
             var address = baseUrl;
+
             // Asynchronously get the document in a new context using the configuration
             var initialRequest = await BrowsingContext.New(config).OpenAsync(address);
-            // This CSS selector gets the desired content
-            var inputSelector = "#aspnetForm input";
-            // Perform the query to get all cells with the content
-            var inputs = initialRequest.QuerySelectorAll(inputSelector);
-            // We are only interested in the text - select it with LINQ
-            //var titles = inputs.Select(m => m.TextContent);
-            foreach (var input in inputs)
+
+            // Uncheck the pagination option. Throw exception if it doesn't exist.
+            GetInput(initialRequest, "ctl00_B_Center_VoturiPlen1_chkPaginare").IsChecked = false;
+            SetHtmlEvent(initialRequest, "ctl00$B_Center$VoturiPlen1$chkPaginare", "");
+
+            var initialBrowser = await SubmitMainForm(initialRequest);
+            LocalLogger.Trace("Finished generating the initial browser state");
+            return initialBrowser;
+        }
+
+        private void SetHtmlEvent(IDocument document, string target, string argument)
+        {
+            GetInput(document, "__EVENTTARGET").Value = target;
+            GetInput(document, "__EVENTARGUMENT").Value = argument;
+        }
+
+        private IHtmlInputElement GetInput(IDocument document, string inputId, bool throwException = true)
+        {
+            var selector = "#" + inputId;
+            var element = document.QuerySelector(selector);
+            if (!throwException)
             {
-                if (!input.HasAttribute("name") || !input.HasAttribute("type") || !input.Attributes["type"].Value.Equals("hidden"))
-                {
-                    continue;
-                }
-                var name = input.Attributes["name"].Value;
-                string value = string.Empty;
-                if (input.HasAttribute("value"))
-                {
-                    value = input.Attributes["value"].Value;
-                }
+                return element as IHtmlInputElement;
             }
 
-            (initialRequest.QuerySelector("#ctl00_B_Center_VoturiPlen1_chkPaginare") as IHtmlInputElement).IsChecked = false;
-            (initialRequest.QuerySelector("#__EVENTTARGET") as IHtmlInputElement).Value = "ctl00$B_Center$VoturiPlen1$chkPaginare";
+            if (element == null || !(element is IHtmlInputElement))
+            {
+                throw new UnexpectedPageContentException("Failed finding input " + selector);
+            }
+            return (IHtmlInputElement)element;
+        }
 
-            return await ((IHtmlFormElement)initialRequest.QuerySelector("#aspnetForm")).SubmitAsync();
+        private async Task<IDocument> SubmitMainForm(IDocument document)
+        {
+            return await ((IHtmlFormElement)document.QuerySelector("#aspnetForm")).SubmitAsync();
         }
     }
 }
---
+++ ScraperLibrary/UnexpectedPageContentException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ro.stancescu.CDep.ScraperLibrary
+{
+    public class UnexpectedPageContentException: Exception
+    {
+        public UnexpectedPageContentException() : base() { }
+
+        public UnexpectedPageContentException(string message) : base(message) { }
+
+        public UnexpectedPageContentException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
