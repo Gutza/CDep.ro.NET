046982f830ec34bb174e5e4d14e6361cf148876f
Bogdan Stancescu <bogdan.stancescu@rbc.com.ro>
2018-07-29 15:42:23 +0300

    Added a generic scraper

---
+++ ScraperLibrary/SenateGenericScraper.cs
@@ -0,0 +1,45 @@
+﻿using AngleSharp.Dom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ro.stancescu.CDep.ScraperLibrary
+{
+    internal class SenateGenericScraper : SenateNetworkScraper
+    {
+        internal string Url { get; private set; }
+
+        internal SenateGenericScraper(string url)
+        {
+            Url = url;
+        }
+
+        protected override string GetBaseUrl()
+        {
+            return Url;
+        }
+
+        internal async Task<IDocument> GetDocument()
+        {
+            var invalid = System.IO.Path.GetInvalidFileNameChars();
+            var cacheId = Url.Replace('/', '!').Replace(':', '.').Replace('?', '@');
+            foreach(char invalidChar in invalid)
+            {
+                cacheId = cacheId.Replace(invalidChar, '-');
+            }
+            var doc = await GetCached(cacheId);
+            if (doc != null)
+            {
+                return doc;
+            }
+
+            var result = await GetLiveBaseDocument();
+
+            SaveCached(cacheId);
+
+            return result;
+        }
+    }
+}
