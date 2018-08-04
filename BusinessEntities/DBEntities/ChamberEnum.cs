d7cb728f7f0a7a0cf16106ab97266bce1bd8298e
Bogdan Stancescu <bogdan.stancescu@rbc.com.ro>
2018-08-04 11:47:06 +0300

    Declared ChamberEnum

---
+++ BusinessEntities/DBEntities/ChamberEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ro.stancescu.CDep.BusinessEntities.DBEntities
+{
+    enum Chambers
+    {
+        InvalidChamber = 0,
+        Deputees,
+        Senators,
+    }
+}
